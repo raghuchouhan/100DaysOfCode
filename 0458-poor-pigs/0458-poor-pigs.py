bfe6a9de2a7cc50e7f1493da6979eca29fedbb6e
Raghuveer singh Chouhan <sondhiya79095@gmail.com>
2023-10-29 14:29:36 +0530

    Time: 38 ms (48%), Space: 16.2 MB (74.4%) - LeetHub

---
+++ 0458-poor-pigs/0458-poor-pigs.py
@@ -0,0 +1,24 @@
+class Solution:
+    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
+        base = minutesToTest // minutesToDie + 1
+        res, p = 0, 1
+        while p < buckets:
+            p *= base
+            res += 1
+        return res
+
+############
+
+class Solution(object):
+    def poorPigs(self, buckets, minutesToDie, minutesToTest):
+        """
+        :type buckets: int
+        :type minutesToDie: int
+        :type minutesToTest: int
+        :rtype: int
+        """
+        tests = minutesToTest / minutesToDie + 1
+        pigs = 0
+        while tests ** pigs < buckets:
+            pigs += 1
+        return pigs    
